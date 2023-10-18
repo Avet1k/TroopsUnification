76f3607f33cdb98ab0dba2f6b9e2818c93b03e90
Avet1k <xavetikx@gmail.com>
2023-10-18 21:09:12 +0300

    Task is ready

--- Program.cs
+++ Program.cs
@@ -3,6 +3,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Легендарное юнайт Виллариба и Виллабаджо!");
+        string letter = "Б";
+        
+        List<Soldier> alphaSquad = new List<Soldier>
+        {
+            new("Пакман"),
+            new("Биг Босс"),
+            new("БТ-7274"),
+            new("Гордон Фриман"),
+            new("Пирамидоголовый"),
+            new ("Лара Крофт")
+        };
+        List<Soldier> bravoSquad = new List<Soldier>
+        {
+            new("Боузер"),
+            new("Байсон М.")
+        };
+        
+        Console.WriteLine("  Отряд Альфа до:");
+        ShowNames(alphaSquad);
+        
+        Console.WriteLine("\n  Отряд Браво до:");
+        ShowNames(bravoSquad);
+        
+        var bNamedSoldiers = alphaSquad.Where(soldier => soldier.Name.StartsWith(letter)).ToList();
+        bravoSquad = bravoSquad.Concat(bNamedSoldiers).ToList();
+        alphaSquad = alphaSquad.Except(bNamedSoldiers).ToList();
+        
+        Console.WriteLine("\n  Отряд Альфа после:");
+        ShowNames(alphaSquad);
+        
+        Console.WriteLine("\n  Отряд Браво после:");
+        ShowNames(bravoSquad);
     }
+
+    private static void ShowNames(List<Soldier> soldiers)
+    {
+        foreach (Soldier soldier in soldiers)
+            Console.WriteLine(soldier.Name);
+    }
+}
+
+class Soldier
+{
+    public Soldier(string name)
+    {
+        Name = name;
+    }
+    
+    public string Name { get; private set; }
 }
